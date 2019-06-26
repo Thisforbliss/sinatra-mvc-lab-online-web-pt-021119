1a59b805e7f4961a66e818bb83ef8560f36a0cc3
Malcolm Leake <echelon1089@gmail.com>
2019-06-26 22:04:19 +0000

    Done.

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,128 +1,24 @@
-class Piglatin
+class PigLatin
   
-  attr_reader :text 
+ def piglatinize(string)
+    a = string.split(" ")
+    b = a.map {|word| piglatinize_word(word)}
+    b.join(" ")
+  end 
   
-  def initialize(text)
-   @text = text.downcase 
+ def piglatinize_word(word)
+    first_letter = word[0].downcase
+    if ["a", "e", "i", "o", "u"].include?(first_letter)
+        "#{word}way"
+    else
+      consonants = []
+     consonants << word[0]
+        if ["a", "e", "i", "o", "u"].include?(word[1]) == false
+          consonants << word[1]
+          if ["a", "e", "i", "o", "u"].include?(word[2]) == false
+            consonants << word[2]
+          end
+        end
+      "#{word[consonants.length..-1] + consonants.join + "ay"}"
+    end
   end
-  
-  vowels = ["a", "e", "i", "o", "u"]
-  two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
-  three_letter_consonants = ["thr", "sch", "squ"]
-  
-  words = text.split(" ")
-  piglatin = []
-  
-  words.each do |word|
-      # Words that start with a vowels
-      if vowels.include? word[0]
-          result.push word << 'ay'
-          
-      # Words that start with a consonant
-      else
-          # Check for special consonants
-          if three_letter_consonants.include? word[0] + word[1] + word[2]
-              # Slice off first three letters
-              first_three_letters = word.slice!(0,3)
-              
-              # Add letters to end of word with 'ay'
-              result.push word << first_three_letters << 'ay'
-              
-          elsif  two_letter_consonants.include? word[0] + word[1]
-              # Slice off first two letters
-              first_two_letters = word.slice!(0,2)
-              
-              # Add the letters to end of word with 'ay'
-              result.push word << first_two_letters << 'ay'
-              
-          else
-              # Slice off first letter...
-              first_letter = word.slice!(0)
-              
-              # Add first letter to end of word with 'ay'
-              result.push word << first_letter << 'ay'
-          end #End of special consonant check
-  
-      end #End of vowel check
-      
-      
-      
-  end #End of words.each
-  
-  
-  
-  #Present the processed words as a single string
-  return result.join(" ")
-
-end #End of translate function
-
-translate('three');
-  
-  
-  
-  
-
-
-end
-
-# def translate(str)
-#   # Vowels to consider
-#   vowels = ["a", "e", "i", "o", "u"]
-  
-#   # Special cases to consider
-#   two_letter_consonants = ["ch", "sh", "qu", "th", "br"]
-#   three_letter_consonants = ["thr", "sch", "squ"]
-
-#   # Seperate each word from the phrase given
-#   words = str.split(" ")
-  
-#   #Location for processed words
-#   result = [];
-  
-  
-  
-#   words.each do |word|
-#       # Words that start with a vowels
-#       if vowels.include? word[0]
-#           result.push word << 'ay'
-          
-#       # Words that start with a consonant
-#       else
-#           # Check for special consonants
-#           if three_letter_consonants.include? word[0] + word[1] + word[2]
-#               # Slice off first three letters
-#               first_three_letters = word.slice!(0,3)
-              
-#               # Add letters to end of word with 'ay'
-#               result.push word << first_three_letters << 'ay'
-              
-#           elsif  two_letter_consonants.include? word[0] + word[1]
-#               # Slice off first two letters
-#               first_two_letters = word.slice!(0,2)
-              
-#               # Add the letters to end of word with 'ay'
-#               result.push word << first_two_letters << 'ay'
-              
-#           else
-#               # Slice off first letter...
-#               first_letter = word.slice!(0)
-              
-#               # Add first letter to end of word with 'ay'
-#               result.push word << first_letter << 'ay'
-#           end #End of special consonant check
-  
-#       end #End of vowel check
-      
-      
-      
-#   end #End of words.each
-  
-  
-  
-#   #Present the processed words as a single string
-#   return result.join(" ")
-
-# end #End of translate function
-
-# translate('three');
-
